344d9321e0598ae726a2df2d6130d218f01553c3
Lincoln Josue <lincoln.martinez@tecsup.edu.pe>
2020-07-13 23:31:50 -0500

    Migraciones creadas productos videos y categorias

---
+++ database/migrations/2020_07_14_002634_create_videos_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVideosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        
+        Schema::create('videos', function (Blueprint $table) {
+            $table->id();
+            $table->string('video_path');
+            $table->string('descripcion');
+            $table->string('resumen');
+            $table->string('genero');
+            $table->string('duracion');
+            $table->double('precio', 8, 2);
+            $table->foreignId('categoria_id');
+            $table->foreign('categoria_id')->references('id')->on('categorias');
+            $table->foreignId('producto_id');
+            $table->foreign('producto_id')->references('id')->on('productos');
+            $table->timestamps();
+        });
+       
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('videos');
+    }
+}
